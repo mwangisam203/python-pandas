b1c2c03f824faa9cdaf631df66ed041f216e6b60
mwangisam203 <mwangisam203@gmail.com>
2026-03-04 04:38:16 -0500

    samples

--- learn.py
+++ learn.py
@@ -1,7 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt 
 
-df = pd.read_json("/home/sam/Downloads/pd-project_small.json")
+df = pd.read_csv("/home/sam/Downloads/pd-big-project.csv", index_col="Order_ID")
 
-print(df.to_string)
-print(df["Date"])
+print(df.to_string())
+
+#Selection by Column
+# print(df.index.is_unique)                 # False means duplicates exist
+# print((df.index == "Helen Smith").sum())  # how many times she appears
+# print(df.loc[990:1000, ["Customer_Name", "Product", "Quantity"]])
+
+
+# #Selection By Rows
+print(df.iloc[0:10, 2:6]) #Selection from rows indicated and then specific columns eg from index 3 to 6
